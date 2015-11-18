17e775bbcf025451a6de2a0c5fe577e0b0ab570d
Janosch Deurer <janosch.deurer@geonautik.de>
2015-11-18 11:59:54 +0100

    Add file for exercise 2a

---
+++ 2a/simple_convolution.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Execise 2a of deep learning lab course
+"""
+
+import numpy as np
+
+# import nn
+
+
+def conv(image, filters, convout):
+    """
+    TODO: implement convolution here
+    """
+    pass
+
+
+def main():
+    """
+    Entrypoint if called as an executable
+    """
+    img = np.asarray([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12],
+                      [13, 14, 15, 16]], dtype=np.float64).reshape(1, 1, 4, 4)
+    # remember the first dimension is the batch size here
+    # lets repeat the image so that we get a more useful test
+    imgs = np.repeat(img, 2, axis=0)
+    print(imgs.shape)
+
+    # to test we will only use one (3,3) filter which does not fits into the
+    # (4,4) images so you need to do padding ;) first dimension is input
+    # channels, second is the number of filters 3rd
+    # and 4th are filter dimensions
+    filters = np.eye(3).reshape((1, 1, 3, 3))
+    print(filters.shape)
+
+    # since we are doing same convolutions the output should be the same size
+    # as the input
+    convout = np.zeros_like(imgs)
+
+    # apply the convolution
+    conv(imgs, filters, convout)
+
+    # print the output and compare to the desired output
+    print(convout)
+
+    real_output = np.asarray(
+        [[[[7., 9., 11., 4.],
+           [15., 18., 21., 11.],
+           [23., 30., 33., 19.],
+           [13., 23., 25., 27.]]],
+
+
+         [[[7., 9., 11., 4.],
+           [15., 18., 21., 11.],
+           [23., 30., 33., 19.],
+           [13., 23., 25., 27.], ]]], dtype=np.float64)
+
+    diff = np.linalg.norm(real_output - convout)
+    # the difference between those should be smaller than eps
+    eps = 1e-4
+    print("Diff {}".format(diff))
+    assert diff < eps
+
+if __name__ == '__main__':
+    main()
