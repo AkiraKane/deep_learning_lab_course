53413d7789b6fd12ae435fb3e7141730b20ea3f0
Janosch Deurer <janosch.deurer@geonautik.de>
2016-01-08 17:30:22 +0100

    Fixes some mistake

--- run.py
+++ run.py
@@ -16,11 +16,11 @@ valid_x, valid_y = valid_data
 test_x, test_y = test_data
 
 train_x = train_x.astype(np.float_) / 255.
-train_y = train_y.astype(np.float_) / 255.
+train_y = train_y.astype(np.float_)
 valid_x = valid_x.astype(np.float_) / 255.
-valid_y = valid_y.astype(np.float_) / 255.
+valid_y = valid_y.astype(np.float_)
 test_x = test_x.astype(np.float_) / 255.
-test_y = test_y.astype(np.float_) / 255.
+test_y = test_y.astype(np.float_)
 
 input_shape = (None, 3, 32, 32)
 layers = [nn.InputLayer(input_shape)]
@@ -28,7 +28,7 @@ layers.append(nn.Conv(
               layers[-1],
               n_feats=2,
               filter_shape=(3, 3),
-              init_stddev=0.01,
+              init_stddev=0.1,
               activation_fun=nn.layers.Activation('relu')))
 layers.append(nn.Flatten(layers[-1]))
 layers.append(nn.FullyConnectedLayer(
@@ -40,7 +40,7 @@ layers.append(nn.FullyConnectedLayer(
               layers[-1],
               num_units=10,
               init_stddev=0.1,
-              activation_fun=nn.layers.Activation('relu')))
+              activation_fun=None))
 layers.append(nn.SoftmaxOutput(layers[-1]))
 net = nn.NeuralNetwork(layers)
 
